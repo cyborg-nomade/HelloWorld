e1244d0006f687f5c0e4e5783c424fd7f431a946
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-02 12:12:40 -0300

    arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,16 +8,27 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var uriel = new Person
-            {
-                FirstName = "Uriel",
-                LastName = "Fiori"
-            };
-            uriel.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+            
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+            
+            var names= new string[3] {"Jack","John", "Mary"};
+
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
+
         }
     }
 }
