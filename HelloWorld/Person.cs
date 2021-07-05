1777537adf8b0ce63638ff3be75cf0a0a553bb0f
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-05 14:02:13 -0300

    conditionals

--- HelloWorld/Person.cs
+++ HelloWorld/Person.cs
@@ -6,6 +6,7 @@ namespace HelloWorld
     {
         public string FirstName;
         public string LastName;
+        public int Age;
 
         public void Introduce()
         {
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,15 +8,58 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var a = 10;
-            var b = a;
-            b++;
-            Console.WriteLine($"a: {a}, b: {b}");
+            int hour = 10;
 
-            var array1 = new int[3] { 1, 2, 3 };
-            var array2 = array1;
-            array2[0] = 0;
-            Console.WriteLine($"array1[0]: {array1[0]}, array2[0]: {array2[0]}");
+            if (hour > 0 && hour < 12)
+            {
+                Console.WriteLine("It's morning.");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine("It's afternoon.");
+            }
+            else
+            {
+                Console.WriteLine("It's evening.");
+            }
+
+            bool isGoldCustomer = true;
+
+            float price;
+
+            //if (isGoldCustomer)
+            //    price = 19.95f;
+            //else
+            //    price = 29.95f;
+
+            price = isGoldCustomer ? 19.95f : 29.95f;
+
+            Console.WriteLine(price);
+
+            var season = Season.Autumn;
+
+            switch (season)
+            {
+                case Season.Autumn:
+                    Console.WriteLine("It's autumn.");
+                    break;
+
+                case Season.Summer:
+                    Console.WriteLine("It's summer.");
+                    break;
+
+                case Season.Spring:
+                    Console.WriteLine("It's spring.");
+                    break;
+
+                case Season.Winter:
+                    Console.WriteLine("It's winter");
+                    break;
+
+                default:
+                    Console.WriteLine("No season");
+                    break;
+            }
         }
     }
 }
