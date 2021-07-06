5ae6709ab2c78ff2d1511bcd2db2298017d031a1
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-06 11:51:58 -0300

    for loops

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,57 +8,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            int hour = 10;
-
-            if (hour > 0 && hour < 12)
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("It's morning.");
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                Console.WriteLine("It's afternoon.");
-            }
-            else
-            {
-                Console.WriteLine("It's evening.");
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
             }
 
-            bool isGoldCustomer = true;
-
-            float price;
-
-            //if (isGoldCustomer)
-            //    price = 19.95f;
-            //else
-            //    price = 29.95f;
-
-            price = isGoldCustomer ? 19.95f : 29.95f;
-
-            Console.WriteLine(price);
-
-            var season = Season.Autumn;
-
-            switch (season)
+            for (int i = 10; i >= 0; i--)
             {
-                case Season.Autumn:
-                    Console.WriteLine("It's autumn.");
-                    break;
-
-                case Season.Summer:
-                    Console.WriteLine("It's summer.");
-                    break;
-
-                case Season.Spring:
-                    Console.WriteLine("It's spring.");
-                    break;
-
-                case Season.Winter:
-                    Console.WriteLine("It's winter");
-                    break;
-
-                default:
-                    Console.WriteLine("No season");
-                    break;
+                if (i%2==0)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
