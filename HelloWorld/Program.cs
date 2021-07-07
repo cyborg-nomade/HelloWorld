70bf5a7f47897b43398ac290c0c4d68c25ef9f73
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-07 11:42:22 -0300

    array methods

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,41 +8,51 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var random = new Random();
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(random.Next());
-            }
-            
-            for (int i = 0; i < 10; i++)
-            {
-                Console.WriteLine(random.Next(1,10));
-            }
+            var numbers = new[] { 3, 7, 9, 2, 14, 6 };
 
-            /*for (int i = 0; i < 10; i++)
+            // Length
+            Console.WriteLine("Length: " + numbers.Length);
+
+            // IndexOf()
+            var index = Array.IndexOf(numbers, 9);
+            Console.WriteLine("Index of 9: "+ index);
+
+            // Clear()
+            Array.Clear(numbers, 0, 2);
+
+            Console.WriteLine("Effect of Clear()");
+            foreach (var n in numbers)
             {
-                Console.Write((char)random.Next(97, 122));
-            }
-            Console.WriteLine();*/
-
-            /*for (int i = 0; i < 10; i++)
-            {
-                Console.Write((char)('a' + random.Next(0,26)));
-            }
-            Console.WriteLine();*/
-
-            const int passwordLength = 8;
-
-            var buffer = new char[passwordLength];
-
-            for (int i = 0; i < passwordLength; i++)
-            {
-                buffer[i] = (char)('a' + random.Next(0,26));
+                Console.WriteLine(n);
             }
 
-            var password = new string(buffer);
+            // Copy()
+            var another = new int[3];
+            Array.Copy(numbers, another, 3);
 
-            Console.WriteLine(password);
+            Console.WriteLine("Effect of Copy()");
+            foreach (var n in another)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Sort()
+            Array.Sort(numbers);
+
+            Console.WriteLine("Effect of Sort()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+
+            // Reverse()
+            Array.Reverse(numbers);
+
+            Console.WriteLine("Effect of Reverse()");
+            foreach (var n in numbers)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
