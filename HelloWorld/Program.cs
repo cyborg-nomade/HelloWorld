f599a34fef3719198d8207ac92078a1aa5900608
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-27 12:47:51 -0300

    timespans

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,22 +9,20 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            var dateTime = new DateTime(2021, 1, 1);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            // Creating
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
 
-            Console.WriteLine("Hour: "+ now.Hour);
-            Console.WriteLine("Minute: " + now.Minute);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration = " + duration);
 
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
 
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("MMMM"));
-            Console.WriteLine(now.ToString("yyyy-MM-dd"));
+            // Properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
         }
     }
 }
