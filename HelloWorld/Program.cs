9a05530d0cae718b6e788eb91d2ff5e992900315
Uriel Fiori <uriel.fiori@gmail.com>
2021-07-29 20:04:20 -0300

    string exercises

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using HelloWorld.Math;
 
@@ -10,164 +12,98 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            /*1- When you post a message on Facebook, depending on the number of people who like your post, Facebook displays different information.
+            /*1- Write a program and ask the user to enter a few numbers separated by a hyphen. Work out if the numbers are consecutive. For example, if the input is "5-6-7-8-9" or "20-19-18-17-16", display a message: "Consecutive"; otherwise, display "Not Consecutive".*/
+            Console.WriteLine("Type a sequence of numbers separated by a dash (-)");
+            var seqSeparatedByDash = Console.ReadLine();
 
-                If no one likes your post, it doesn't display anything.
-                If only one person likes your post, it displays: [Friend's Name] likes your post.
-                If two people like your post, it displays: [Friend 1] and [Friend 2] like your post.
-                If more than two people like your post, it displays: [Friend 1], [Friend 2] and [Number of Other People] others like your post.
-                Write a program and continuously ask the user to enter different names, until the user presses Enter (without supplying a name). Depending on the number of names provided, display a message based on the above pattern.*/
-
-            var isExit = false;
-            var namesList = new List<string>()
+            if (!string.IsNullOrWhiteSpace(seqSeparatedByDash))
             {
+                var numbers = seqSeparatedByDash.Split('-');
+                var firstNumber = int.Parse(numbers[0]);
+                var notConsecutive = false;
 
-            };
-
-            while (!isExit)
-            {
-                Console.WriteLine("Type a name: ");
-                var newName = Console.ReadLine();
-
-                if (newName!="")
+                for (int i = 1; i < numbers.Length; i++)
                 {
-                    namesList.Add(newName);
-                }
-                else
-                {
-                    isExit = true;
-                }
-            }
-
-            switch (namesList.Count)
-            {
-                case 0:
-                    Console.WriteLine("");
-                    break;
-
-                case 1:
-                    Console.WriteLine(namesList[0] + " likes your post");
-                    break;
-
-                case 2:
-                    Console.WriteLine(namesList[0] + " and " + namesList[1] + " like your post");
-                    break;
-
-                default:
-                    Console.WriteLine(namesList[0] + " and " + namesList[1] + " and " + (namesList.Count-2) + " other people like your post");
-                    break;
-            }
-
-            /*2- Write a program and ask the user to enter their name. Use an array to reverse the name and then store the result in a new string. Display the reversed name on the console.*/
-
-            Console.WriteLine("Write your name: ");
-            var input = Console.ReadLine();
-            if (input != null)
-            {
-                var name = input.ToCharArray();
-                Array.Reverse(name);
-                var reversedName = new string(name);
-                Console.WriteLine(reversedName + " is your reversed name");
-            }
-
-            /*3- Write a program and ask the user to enter 5 numbers. If a number has been previously entered, display an error message and ask the user to re-try. Once the user successfully enters 5 unique numbers, sort them and display the result on the console.*/
-
-            var numbers = new int[5];
-
-            for (int i = 0; i < 5; i++)
-            {
-                Console.WriteLine("Enter a new number: ");
-                var inputNumber = Convert.ToInt32(Console.ReadLine());
-                while (numbers.Contains(inputNumber))
-                {
-                    Console.WriteLine("This number has already been entered, type another: ");
-                    inputNumber = Convert.ToInt32(Console.ReadLine());
-                }
-
-                numbers[i] = inputNumber;
-            }
-
-            Array.Sort(numbers);
-            Console.WriteLine("This is your sorted array: ");
-            foreach (var n in numbers)
-            {
-                Console.WriteLine(n);
-            }
-
-            /*4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.*/
-
-            var isQuit = false;
-            var numbersList = new List<int>()
-            {
-
-            };
-
-            while (!isQuit)
-            {
-                Console.WriteLine("Type a number or QUIT to exit: ");
-                var inputString = Console.ReadLine();
-                if (inputString == "QUIT")
-                {
-                    isQuit = true;
-                }
-                else
-                {
-                    numbersList.Add(Convert.ToInt32(inputString));
-                }
-            }
-
-            var displayNumbersList = new List<int>();
-
-            foreach (var unique in numbersList)
-            {
-                if (!displayNumbersList.Contains(unique))
-                {
-                    displayNumbersList.Add(unique);
-                }
-            }
-
-            foreach (var display in displayNumbersList)
-            {
-                Console.WriteLine(display);
-            }
-
-            /*5- Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.*/
-            var isValid = false;
-
-            while (!isValid)
-            {
-                Console.WriteLine("Type a list of 5 comma-separated numbers");
-                var inputString2 = Console.ReadLine();
-
-                var numberList = new List<int>()
-                {
-
-                };
-
-                if (inputString2 != "")
-                {
-                    var inputStringArray = inputString2.Split(',');
-                    foreach (var s in inputStringArray)
+                    if (firstNumber+i!=int.Parse(numbers[i]))
                     {
-                        numberList.Add(Convert.ToInt32(s));
+                        notConsecutive = true;
                     }
                 }
-                else
+
+                Console.WriteLine(notConsecutive ? "Not Consecutive" : "Consecutive");
+            }
+            else
+            {
+                Console.WriteLine("Empty sequence");
+            }
+
+            /*2- Write a program and ask the user to enter a few numbers separated by a hyphen. If the user simply presses Enter, without supplying an input, exit immediately; otherwise, check to see if there are duplicates. If so, display "Duplicate" on the console.*/
+
+            Console.WriteLine("Type a sequence of numbers separated by a dash (-)");
+            var seqSeparatedByDash2 = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(seqSeparatedByDash2))
+            {
+                var numbers = seqSeparatedByDash2.Split('-');
+                bool hasDuplicate = numbers.Length != numbers.Distinct().Count();
+
+                Console.WriteLine(hasDuplicate ? "Duplicate" : "No Duplicates");
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+
+            /*3- Write a program and ask the user to enter a time value in the 24-hour time format (e.g. 19:00). A valid time should be between 00:00 and 23:59. If the time is valid, display "Ok"; otherwise, display "Invalid Time". If the user doesn't provide any values, consider it as invalid time.*/
+
+            Console.WriteLine("Type a valid 24-hour time hour (00:00 - 23:59)");
+            var timeValue = Console.ReadLine();
+            DateTime convertedTime;
+
+            Console.WriteLine(DateTime.TryParseExact(timeValue, "HH:mm", new CultureInfo("en-US"), DateTimeStyles.None,
+                out convertedTime)
+                ? "Ok"
+                : "Invalid Time");
+
+
+            /*4- Write a program and ask the user to enter a few words separated by a space. Use the words to create a variable name with PascalCase. For example, if the user types: "number of students", display "NumberOfStudents". Make sure that the program is not dependent on the input. So, if the user types "NUMBER OF STUDENTS", the program should still display "NumberOfStudents".*/
+
+            Console.WriteLine("Type a few words separated by space");
+            var seqWords = Console.ReadLine();
+
+            if (!string.IsNullOrWhiteSpace(seqWords))
+            {
+                var resultString = seqWords.ToLower();
+                TextInfo info = CultureInfo.CurrentCulture.TextInfo;
+                resultString = info.ToTitleCase(resultString).Replace(" ", string.Empty);
+                Console.WriteLine(resultString);
+            }
+            else
+            {
+                Console.WriteLine("Empty input");
+            }
+
+
+            /*5- Write a program and ask the user to enter an English word. Count the number of vowels (a, e, o, u, i) in the word. So, if the user enters "inadequate", the program should display 6 on the console.*/
+
+            Console.WriteLine("Type one word in English");
+            var oneWord = Console.ReadLine();
+            var ctVowel = 0;
+
+            if (!string.IsNullOrWhiteSpace(oneWord)){
+                foreach (var c in oneWord)
                 {
-                    numberList.Clear();
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        ctVowel++;
+                    }
                 }
 
-                if (numberList.Count < 5 || !numberList.Any())
-                {
-                    Console.WriteLine("Invalid List, try again");
-                    continue;
-                }
-                else
-                {
-                    isValid = true;
-                    numberList.Sort();
-                    Console.WriteLine("The three smallest numbers are:" + numberList[0] + ","+ numberList[1]+","+numberList[2]);
-                }
+                Console.WriteLine(ctVowel);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
             }
         }
     }
